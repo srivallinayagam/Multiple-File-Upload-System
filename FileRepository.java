41621feac47352f3c103868ef1e573720cd4ffd6
Sri Vallinayagam V.S. <151116310+srivallinayagam@users.noreply.github.com>
2024-10-03 10:29:45 +0530

    Delete FileRepository.java

--- FileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.multifileupload;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FileRepository extends MongoRepository<FileEntity, String> {
-}
